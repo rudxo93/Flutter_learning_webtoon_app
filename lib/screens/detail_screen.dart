03610de9c8a97b6f4cf01ea9bfbdb99c9706ecf5
GyoungTaeKim <kim3jjj@naver.com>
2024-06-20 14:21:06 +0900

    Futures 1. Episode와 해당 toon의 정보를 detail_screen에서 사용할 준비

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DetailScreen extends StatelessWidget {
+import '../models/webtoon_detail_model.dart';
+import '../models/webtoon_episode_model.dart';
+import '../services/api_service.dart';
+
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreen(
       {super.key, required this.title, required this.thumb, required this.id});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +35,13 @@ class DetailScreen extends StatelessWidget {
         elevation: 2,
         // 하단 음영
         centerTitle: true,
-        backgroundColor: Colors.white, // 배경
-        foregroundColor: Colors.green, // text Color 및 좌측에 위치한 아이콘 색상도 반영
+        backgroundColor: Colors.white,
+        // 배경
+        foregroundColor: Colors.green,
+        // text Color 및 좌측에 위치한 아이콘 색상도 반영
 
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(fontSize: 24),
         ),
       ),
@@ -32,7 +54,7 @@ class DetailScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   width: 250,
                   clipBehavior: Clip.hardEdge, // 자식의 부모 영역 침범 제어
@@ -44,7 +66,7 @@ class DetailScreen extends StatelessWidget {
                             offset: Offset(10, 10),
                             color: Colors.black.withOpacity(0.5))
                       ]),
-                  child: Image.network(thumb),
+                  child: Image.network(widget.thumb),
                 ),
               ),
             ],
